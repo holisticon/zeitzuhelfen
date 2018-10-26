327d99b7ab1e92351ef84280da713ba905af5410
Jannik Arndt <jannik@jannikarndt.de>
2018-10-26 15:20:35 +0200

    Fixed login in Firefox

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/apps/zeitzuhelfen'));
 
+
+app.get('/authentication/callback', function(req, res) {
+	res.sendFile(path.join(__dirname + '/dist/apps/zeitzuhelfen/index.html'));
+});
+
 app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/dist/apps/zeitzuhelfen/index.html'));
 });
